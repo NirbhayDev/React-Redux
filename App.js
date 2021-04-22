5ad084cd53b6bfbf4a3a8f220fdc2a5e2b7727f3
ReactLearnNirbhay <82522128+ReactLearnNirbhay@users.noreply.github.com>
2021-04-22 11:46:02 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import 'react-native-gesture-handler';
+import AppNavContainer from './src/Navigation/index';
+
+const App = () => {
+  return (
+   <AppNavContainer>
+   </AppNavContainer>
+  );
+};
+
+
+
+export default App;
